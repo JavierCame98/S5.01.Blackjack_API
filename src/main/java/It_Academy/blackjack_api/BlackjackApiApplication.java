6feb146de4e92a4b9443db3fd8e8c8c99d359a86
Javier Castillo <javiercame98@gmail.com>
2026-04-28 10:32:49 +0200

    feat: initial commit

---
+++ src/main/java/It_Academy/blackjack_api/BlackjackApiApplication.java
@@ -0,0 +1,13 @@
+package It_Academy.blackjack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackApiApplication.class, args);
+	}
+
+}
